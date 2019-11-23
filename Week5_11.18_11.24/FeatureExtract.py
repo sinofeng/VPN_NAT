e96533a5d7af00cd1c388f7d2d19c5a16d255179
henryguo <11612812@mail.sustech.edu.cn>
2019-11-23 23:39:55 +0800

    update_ongoogle

--- Week5_11.18_11.24/FeatureExtract.py
+++ Week5_11.18_11.24/FeatureExtract.py
@@ -3,9 +3,6 @@ import pickle
 
 SEPERATOR = "+"
 
-class flow_container():
-    def __init__(self):
-        self.multiple_flows = {}
 
 class flow():
     def __init__(self,packet):
@@ -23,12 +20,16 @@ class flow():
         pass
 
 class flow_packet():
-    def __init__(self,src_ip,src_port,dest_ip,dest_port,protocol_name):
+    def __init__(self,src_ip,src_port,dest_ip,dest_port,protocol_name,timestamp_delta, timestamp_relative, original_pkt):
         self.src_ip = src_ip
         self.src_port = src_port
         self.dest_ip = dest_ip
         self.dest_port = dest_port
         self.protocl_name = protocol_name
+        self.timestamp_delta = timestamp_delta
+        self.timestamp_relative = timestamp_relative
+        self.original_pkt = original_pkt
+
     def sort_ip(self):
         # need to be tested
         # sort the src ip and dest ip as the ascending order
@@ -46,12 +47,12 @@ class flow_packet():
 
 def store_obj2pickle(data,path):
     with open(path,'bw') as f:
-        pickle.dump(data,path)
+        pickle.dump(data,f)
     print("Store data into "+path)
 
 def load_pickle2obj(path):
     with open(path,'br') as f:
-        data = pickle.load(path)
+        data = pickle.load(f)
     print("Load data from "+path)
     return data
 
@@ -60,27 +61,31 @@ def loadpcap_from_file(file_path,filter):
 
 def get_five_element(packet):
     return str(packet.ip.src),str(packet.tcp.srcport),str(packet.ip.dst),str(packet.tcp.dstport),str(packet.ip.proto)
+def get_time_element(packet):
+    return str(packet.tcp.time_delta),str(packet.tcp.time_relative)
 
 if __name__ == '__main__':
-    file_path = 'D:\\vpn\\result\\psiphon_11times.pcap'
+    file_path = 'C:\\Users\\\haoruikwok\\\Desktop\\result_google\\psiphon_11times.pcap'
     pcap_data = loadpcap_from_file(file_path,'tcp')
 
-    flow_container = flow_container()
+    multiple_flows = {}
     pkt_counter = 1
     for packet in pcap_data:
         src_ip,src_port,dst_ip,dst_port,protocol_name = get_five_element(packet)
+
         if src_port == '3389' or dst_port == '3389':
             pkt_counter += 1
             continue
         else:
-            pkt = flow_packet(src_ip,src_port,dst_ip,dst_port,protocol_name)
+            timestamp_delta, timestamp_relative = get_time_element(packet)
+            pkt = flow_packet(src_ip,src_port,dst_ip,dst_port,protocol_name,timestamp_delta,timestamp_relative,packet)
             pkt_quintuple = pkt.sort_quintuple()
-            if pkt_quintuple in flow_container.multiple_flows.keys():
-                flow_container.multiple_flows[pkt_quintuple].packets.append(pkt)
+            if pkt_quintuple in multiple_flows.keys():
+                multiple_flows[pkt_quintuple].packets.append(pkt)
             else:
-                flow_container.multiple_flows[pkt_quintuple] = flow(pkt)
+                multiple_flows[pkt_quintuple] = flow(pkt)
         pkt_counter += 1
-        if pkt_counter%1000 == 0:
+        if pkt_counter % 1000 == 0:
             print("Processing pkt count: ",pkt_counter)
-
-    store_obj2pickle(flow_container,'./flows.pickle')
+    debug = 1
+    store_obj2pickle(multiple_flows,'./flows.pickle')
